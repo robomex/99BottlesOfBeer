b84aabdb58f4ae317cded28eef119913677938e9
robomex <atmorales@gmail.com>
2014-09-27 23:10:05 -0500

    background changes

--- 99BottlesOfBeer/ViewController.swift
+++ 99BottlesOfBeer/ViewController.swift
@@ -13,13 +13,19 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        for var i = 99; i > 0; i-- {
+            if i > 0 {
+                println("\(i) bottles of beer on the wall")
+            }
+        }
+        println("No more bottles of beer on the wall!")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
